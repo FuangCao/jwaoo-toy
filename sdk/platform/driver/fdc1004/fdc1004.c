4cf2c2008aaaa7cae61d97f727df8d577b97afc4
Fuang.Cao <cavan.cfa@gmail.com>
2016-08-06 16:08:06 +0800

    update fdc1004 driver
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- sdk/platform/driver/fdc1004/fdc1004.c
+++ sdk/platform/driver/fdc1004/fdc1004.c
@@ -6,6 +6,9 @@
 #include "fdc1004.h"
 #include "uart.h"
 
+#define FDC1004_GAIN		0xFFFF
+#define FDC1004_CAPDAC		7
+
 static int fdc1004_read_u16(uint8_t addr, uint16_t *value)
 {
 	int ret;
@@ -130,13 +133,13 @@ bool fdc1004_set_enable(bool enable)
 		}
 
 		for (i = 0; i < 4; i++) {
-			ret = fdc1004_write_u16(FDC1004_REG_CONF_MEAS1 + i, i << 13 | 4 << 10 | 1 << 5);
+			ret = fdc1004_write_u16(FDC1004_REG_CONF_MEAS1 + i, i << 13 | 4 << 10 | FDC1004_CAPDAC << 5);
 			if (ret < 0) {
 				println("Failed to fdc1004_write_u16: %d", ret);
 				return false;
 			}
 
-			ret = fdc1004_write_u16(FDC1004_REG_GAIN_CAL_CIN1 + i, 0xFFFF); // 1 << 14);
+			ret = fdc1004_write_u16(FDC1004_REG_GAIN_CAL_CIN1 + i, FDC1004_GAIN);
 			if (ret < 0) {
 				println("Failed to fdc1004_write_u16: %d", ret);
 				return false;
